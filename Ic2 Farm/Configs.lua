7b7256ae5f02109a762bc1f53f06495a307b6013
kla365-Real <tongkrast@gmail.com>
2026-04-12 23:45:48 +0700

    Add files via upload

---
+++ Ic2 Farm/Configs.lua
@@ -0,0 +1,12 @@
+local M = {
+    Dislocation = {x,y,z},   -- location of dislocation
+    StoragePos1 = {x,y,z},   -- corner of Storage
+    StoragePos2 = {x,y,z},   -- corner of Storage
+    FarmPos1 = {x,y,z},      -- corner of Farm
+    FarmPos2 = {x,y,z},      -- corner of Farm
+    Chargerlocation = {x,y,z},-- charger position
+    Plotlength = x           -- num plot
+    Plotwide = x             -- num plot
+    Backlist = ""            -- plant Backlist for Crossbreed
+}  
+return(M)
